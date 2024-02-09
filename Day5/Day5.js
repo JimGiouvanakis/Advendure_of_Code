c4efcf3ddaec9d84cb2a7415f4027416bb5844d3
JimGiouvanakis <106709373+JimGiouvanakis@users.noreply.github.com>
2024-02-09 02:20:35 +0200

    Progress

--- Day5/Day5.js
+++ Day5/Day5.js
@@ -1,29 +1,29 @@
 const fs = require(`fs`);
 
-
 fs.readFile("Day5.txt", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-const lines = data.replace(/\r/g,""); //
-// console.log({lines})
-  const [stack,moves] = lines.split("\n\n");
-  
-  const something = stack.split('\n').map((line) => [...line].filter((value, index) => index % 4 === 1));
-
-//   console.log(something[0]); 
-let FullStack= {};
-  const columns = something.pop();
-for (const line of something) {
-   for (let i = 0; i <columns.length; i++) {
-    if (line[i] !== ` `) {
-        FullStack[columns[i]] = line[i];
-    // console.log(FullStack);
+  if (err) {
+    console.error(err);
+    return;
   }
 
-}
-console.log(FullStack);
-}
-})
+  const lines = data.replace(/\r/g, ""); //
+  // console.log({lines})
+  const [stack, moves] = lines.split("\n\n");
+
+  const something = stack
+    .split("\n")
+    .map((line) => [...line].filter((value, index) => index % 4 === 1));
+
+  // console.log(something[8]);
+  let FullStack = {};
+  const columns = something.pop();
+  console.log(columns);
+  // for (const line of something) {
+  //   for (let i = 0; i < columns.length; i++) {
+  //     if (line[i] !== ` `) {
+  //       FullStack[columns[i]] = line[i];
+  //       // console.log(FullStack);
+  //     }
+  //   }
+  // console.log(FullStack[0]);
+});
